4b3876b80df173082dcf019d1a13d802cdf31ce0
Non169 <51470887+Non169@users.noreply.github.com>
2019-06-28 11:46:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,10 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
-    return '',200
+   # sendText(user,userText)
+   
+    
+   return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
