70e3a7c460ff34f932fd246380f8d8a4148c9259
Non169 <51470887+Non169@users.noreply.github.com>
2019-07-05 11:32:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-   # sendText(user,userText)
-   if (userText == 'สวัดดี') :
-     sendText(user,'สวัดดีเธอชื่ออ่ะไรหรอ')
-   elif (userText == 'ฉันชื่อดอร่า') :
-     sendText(user,'โอเคร')
-   elif (userText == 'ถ้าเห็นหมาป่าให้พูดว่าไสวเปอร์') :
-     sendText(user,'ไสวเปอร์')
+    #sendText(user,userText)
+    if (userText == 'สวัดดี') :
+        sendText(user,'สวัดดีเธอชื่ออ่ะไรหรอ')
+    elif (userText == 'ฉันชื่อดอร่า') :
+        sendText(user,'โอเคร')
+    elif (userText == 'ถ้าเห็นหมาป่าให้พูดว่าไสวเปอร์') :
+        sendText(user,'ไสวเปอร์')
     
    return '',200
 
